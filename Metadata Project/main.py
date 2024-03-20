6cc12de6c90fcb1e99391e451a37d71ea554823d
yatinbichala <110629503+yatinbichala@users.noreply.github.com>
2024-03-20 19:38:10 -0400

    parses language, buggy duplicate checking

--- Metadata Project/main.py
+++ Metadata Project/main.py
@@ -4,6 +4,7 @@
 import IPython
 from IPython.display import display
 import pandas as pd
+from fuzzywuzzy import fuzz 
 
 '''''''''''''''''''''
 Class Declaration
@@ -28,6 +29,8 @@ from dataclasses import dataclass
 
 #@dataclass
 works_list = []
+works_list_japanese = []
+unique_works = []
 class Work:
     def __init__(self, htid, access, rights, ht_bib_key, description, source,
                  source_bib_num, oclc_num, isbn, issn, lccn, title, imprint,
@@ -75,19 +78,27 @@ class Work:
     #pub place 17
     # lang 18
 def parse():
-    with open('sample_1.txt') as input:
+    f = open('japanese.txt', 'w')
+    with open('128MB.txt') as input:
         lines = input.readlines() # list containing lines of file
         for line in lines:
             elem = line.split('\t',25) #array of all information until next work
             #print(elem[17])
+            #print(line)
             #nested loop for rest of elements
-            work = Work(elem[0], elem[1], elem[2], elem[3], elem[4], elem[5], elem[6], elem[7], elem[8], elem[9], elem[10], elem[11], elem[12], elem[13],
-                    elem[14], elem[15], elem[16], elem[17], elem[18],elem[19],elem[20],elem[21],elem[22],elem[23],elem[24],elem[25], False) #replace False with isDupe() func
+            work = Work(elem[0], elem[1], elem[2], elem[3], elem[4], elem[5], elem[6], 
+                        elem[7], elem[8], elem[9], elem[10], elem[11], elem[12], elem[13],
+                        elem[14], elem[15], elem[16], elem[17], elem[18],elem[19],elem[20], 
+                        elem[21],elem[22],elem[23],elem[24],elem[25], False) #replace False with isDupe() func
             works_list.append(work)
-            
-            #print(works_list)
+            if (work.lang == 'jpn'):
+                f.write(str(line)) #to test if it actually writes
+                works_list_japanese.append(work) #list of japanese works
+            # print(i)
+            # i = i + 1
             #bucketing
             #line.split('\t') is used for splitting by tabs
+    f.close()
     
     
 #save relevant fields into class
@@ -125,16 +136,70 @@ bool isDuplicate(work1, work2){
         return false
     if work1.title == work2.title:
         return false;
-    if work1.publication == work2.publication:
-        return false;
     return true;
 }
+
+def isDupe(work1, work2):
+    #Comparing the Title, Author name, and place of publication
+    #0.95 means 95% similar
+    if (fuzz.ratio(work1.author, work2.author) > 0.95 and 
+        fuzz.ratio(work1.title, work2.title) > 0.95 and 
+        fuzz.ratio(work1.publication, work2.publication) > 0.95):
+        return True
+    else: 
+        return False
+#import fuzzywuzzy will also work as it uses the Levesthein Distance algorithm
+#Levesthein distance works if the works are in the same language (as long as both are romanized, it should work)
+    #should note that the percentage that is the same will change if the language is romanized
 '''
 
+def isDuplicate(work1, work2):
+    if work1.author == work2.author and work1.title == work2.title:
+        return True
+    return False
 
+def isDupe(work1, work2):
+    #Comparing the Title, Author name, and place of publication
+    #0.95 means 95% similar
+    if (fuzz.ratio(work1.author, work2.author) > 0.95 and 
+        fuzz.ratio(work1.title, work2.title) > 0.95):
+        #fuzz.ratio(work1.rights_timestamp, work2.rights_timestamp) > 0.50
+        return True
+    else: 
+        return False
+
+def printUniqueList(list):
+    f = open("Unique_works.txt", "w")
+    for n in range(len(list)):
+        f.write(list[n].author + '\t' + list[n].title + '\n')
+
+def removeDupe(list, list2): #list is a list of all works (duplicates included), list2 is an empty list of unique works
+    list2.append(list[0]) #the first is guaranteed to be unique
+    f = open("print_statements.txt", "w")
+    f.write("1" + '\n')
+    for n in range(len(list)):  #where c = n + 1
+        f.write("2" + '\n')
+        for c in range(len(list)):
+            f.write("3" + '\n')
+            if (n == c or c < n): # c <= n
+                f.write("4" + '\n')
+                continue
+            if (isDuplicate(list[n], list[c])):
+                f.write("5" + '\n')
+                continue
+            elif (not isDuplicate(list[n], list[n - 1]) and n != 0):
+                f.write("6" + '\n')
+                list2.append(list[n])
+
+    printUniqueList(list2)
+    #print("7")            
+    #print(list2[0].author)
+
+            
 def main():
     parse()
-    print(works_list[0].author)
+    removeDupe(works_list_japanese, unique_works)
+
     #parse
     #bucket
     #output
