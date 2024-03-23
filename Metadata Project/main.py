a3ec62a4846417b54ffbf5974011083569bb3586
yatinbichala <110629503+yatinbichala@users.noreply.github.com>
2024-03-23 12:51:28 -0400

    Successful uniquelist, left failed removeDupe()

--- Metadata Project/main.py
+++ Metadata Project/main.py
@@ -31,6 +31,7 @@ from dataclasses import dataclass
 works_list = []
 works_list_japanese = []
 unique_works = []
+total_num_of_works = 0
 class Work:
     def __init__(self, htid, access, rights, ht_bib_key, description, source,
                  source_bib_num, oclc_num, isbn, issn, lccn, title, imprint,
@@ -78,8 +79,11 @@ class Work:
     #pub place 17
     # lang 18
 def parse():
-    f = open('japanese.txt', 'w')
-    with open('128MB.txt') as input:
+    #TODO: add user input for file name
+    #global total_num_of_works
+    total_num_of_works = 0
+    f = open('output.txt', 'w')
+    with open('sample_1.txt') as input:
         lines = input.readlines() # list containing lines of file
         for line in lines:
             elem = line.split('\t',25) #array of all information until next work
@@ -91,6 +95,7 @@ def parse():
                         elem[14], elem[15], elem[16], elem[17], elem[18],elem[19],elem[20], 
                         elem[21],elem[22],elem[23],elem[24],elem[25], False) #replace False with isDupe() func
             works_list.append(work)
+            total_num_of_works += 1
             if (work.lang == 'jpn'):
                 f.write(str(line)) #to test if it actually writes
                 works_list_japanese.append(work) #list of japanese works
@@ -161,45 +166,80 @@ def isDuplicate(work1, work2):
 def isDupe(work1, work2):
     #Comparing the Title, Author name, and place of publication
     #0.95 means 95% similar
-    if (fuzz.ratio(work1.author, work2.author) > 0.95 and 
-        fuzz.ratio(work1.title, work2.title) > 0.95):
+    if (fuzz.ratio(work1.author, work2.author) > 90 and 
+        fuzz.ratio(work1.title, work2.title) > 90):
         #fuzz.ratio(work1.rights_timestamp, work2.rights_timestamp) > 0.50
         return True
     else: 
         return False
 
-def printUniqueList(list):
-    f = open("Unique_works.txt", "w")
+def printUniqueList(list, num_unique):
+    f = open("Unique_works_gpt.txt", "w")
+    #total_num_works is not working correctly
+    f.write("From " + str(total_num_of_works) + " to " + str(num_unique) + '\n')
     for n in range(len(list)):
         f.write(list[n].author + '\t' + list[n].title + '\n')
 
-def removeDupe(list, list2): #list is a list of all works (duplicates included), list2 is an empty list of unique works
-    list2.append(list[0]) #the first is guaranteed to be unique
-    f = open("print_statements.txt", "w")
-    f.write("1" + '\n')
-    for n in range(len(list)):  #where c = n + 1
-        f.write("2" + '\n')
-        for c in range(len(list)):
-            f.write("3" + '\n')
-            if (n == c or c < n): # c <= n
-                f.write("4" + '\n')
-                continue
-            if (isDuplicate(list[n], list[c])):
-                f.write("5" + '\n')
-                continue
-            elif (not isDuplicate(list[n], list[n - 1]) and n != 0):
-                f.write("6" + '\n')
-                list2.append(list[n])
+# def removeDupe(list, unique): #list is a list of all works (duplicates included), list2 is an empty list of unique works
+#     unique.append(list[0]) #the first is guaranteed to be unique
+#     f = open("print_statements.txt", "w")
+#     num_unique = 0
+#     # f.write("1" + '\n')
+#     # for n in range(len(list)):  #where c = n + 1
+#     #     f.write("2" + '\n')
+#     #     for c in range(len(list)):
+#     #         f.write("3" + '\n')
+#     #         if (n == c or c < n): # c <= n
+#     #             f.write("4" + '\n')
+#     #             continue
+#     #         if (isDuplicate(list[n], list[c])):
+#     #             f.write("5" + '\n')
+#     #             continue
+#     #         elif (not isDuplicate(list[n], list[n - 1]) and n != 0):
+#     #             f.write("6" + '\n')
+#     #             unique.append(list[n])
+        
 
-    printUniqueList(list2)
-    #print("7")            
-    #print(list2[0].author)
+# def removeDupe(list, unique):
+#     #unique.append(list[0])  # The first element is guaranteed to be unique
+#     num = 0 #for testing
+#     num_unique = 0
+#     for item in list:
+#         is_duplicate = False
+#         print(num)
+#         num+=1
+#         for unique_item in unique:
+#             if isDupe(item, unique_item):
+#                 is_duplicate = True
+#                 break
+#         if not is_duplicate:
+#             unique.append(item)
+#             num_unique += 1
+    
+#     printUniqueList(unique, num_unique)
+#     return
 
-            
+
+def removeDupe(list, unique):
+    #unique.append(list[0])  # The first element is guaranteed to be unique
+    #list.sort(key=lambda x: (x.author, x.title))  # Sort the list based on author and title
+    
+    # Iterate through the sorted list and compare adjacent works
+    num = 0
+    for i in range(0, len(list)):
+        print(num)
+        num+=1
+        if not isDupe(list[i], list[i - 1]):
+            unique.append(list[i])
+
+    num_unique = len(unique)
+    printUniqueList(unique, num_unique)
+
 def main():
     parse()
-    removeDupe(works_list_japanese, unique_works)
-
+    removeDupe(works_list, unique_works)
+    
+    #TODO: add output saying how many duplicates/works left
     #parse
     #bucket
     #output
